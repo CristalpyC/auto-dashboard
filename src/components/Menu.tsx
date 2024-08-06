108f41ad08e390a95dc3a406254231014d0f6db7
cristalPyC <cristaltn03@gmail.com>
2024-08-06 11:23:24 -0400

    Fixed UI bugs

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -5,7 +5,7 @@ interface MenuProps {
 }
 export const Menu: FC<MenuProps> = ({ children }) => {
   return (
-    <nav className="text-2xl flex p-4 bg-[#165ECA] justify-between text-white">
+    <nav className="text-2xl flex p-4 bg-[#3b79d6] justify-between text-white sticky top-0 z-20">
         <h1>Dealw</h1>
         {children}
     </nav>
--- src/components/home-components/Intro.tsx
+++ src/components/home-components/Intro.tsx
@@ -9,26 +9,26 @@ export const Intro = () => {
   return (
     <>
       <Menu>
-          <a href="/login" className="hover:bg-[#6393db2d] p-2">Sign in</a>
+          <a href="/login" className="hover:bg-[#638fff96] hover:shadow-md p-2">Sign in</a>
       </Menu>
       <Zoom>
       <div className="p-11 flex-col lg:flex-row md:flex-row flex justify-center items-center lg:justify-between md:justify-between">
         <div className="mt-5 lg:mt-24 md:mt-24 text-center lg:text-left md:text-left">
-          <h2 className="text-[8vmin] md:text-[7vmin] lg:text-[9vmin] mb-3 text-[#165ECA] font-medium">
+          <h2 className="text-[8vmin] md:text-[6vmin] lg:text-[9vmin] mb-3 text-[#165ECA] font-medium">
             Elevate your
           </h2>
-          <h3 className="text-[7vmin] md:text-[6vmin] lg:text-[7vmin] font-medium">
+          <h3 className="text-[7vmin] md:text-[5vmin] lg:text-[7vmin] font-medium">
             Dealership operations
           </h3>
-          <button onClick={() => router.push('/register')} className="text-[4vmin] md:text-[3vmin] lg:text-[3vmin] md:w-[15rem] lg:w-[18rem] mt-8 text-white  bg-[#6393DB] p-2 shadow-md ease-in-out hover:scale-105 hover:bg-[#547bb5aa]">
+          <button onClick={() => router.push('/register')} className="text-[4vmin] md:text-[2.5vmin] lg:text-[3vmin] md:w-[15rem] lg:w-[18rem] mt-8 text-white  bg-[#6393DB] p-2 shadow-md shadow-blue-700 ease-in-out hover:scale-105 hover:bg-[#547bb5aa]">
             Create your account
           </button>
         </div>
 
         <img
-          src="/dashb-icon.png"
+          src="3d-financial-report-chart-png.webp"
           alt="dashboard-icon"
-          className="mt-11 -rotate-12 w-[80%] lg:w-[50%] md:w-[60%]"
+          className="mt-11 w-[80%] lg:w-[50%] md:w-[45%]"
         />
       </div>
       </Zoom>
